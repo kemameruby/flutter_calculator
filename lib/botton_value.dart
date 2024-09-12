5de3f684aa40275e75cc5ed7d1f1b3103beba28f
Kemamaeruby <preshnelly82@gmail.com>
2024-09-12 12:22:36 +0100

    cal finished

---
+++ lib/botton_value.dart
@@ -0,0 +1,45 @@
+class Btn {
+ static const String del = "Ac";
+ static const String clr = "+/-";
+ static const String per = "%";
+ static const String multiply = "x";
+ static const String divid = "/";
+ static const String add = "+";
+ static const String subtract = "-";
+ static const String calculate = "=";
+ static const String dot = ".";
+
+ static const String n0 = "0";
+ static const String n1 = "1";
+ static const String n2 = "2";
+ static const String n3 = "3";
+ static const String n4 = "4";
+ static const String n5 = "5";
+ static const String n6 = "6";
+ static const String n7 = "7";
+ static const String n8 = "8";
+ static const String n9 = "9";
+
+ static const List<String> buttonValue = [
+   del,
+   clr,
+   per,
+   multiply,
+   n7,
+   n8,
+   n9,
+   divid,
+   n4,
+   n5,
+   n6,
+   subtract,
+   n1,
+   n2,
+   n3,
+   add,    
+   n0,
+   dot,
+  calculate,
+  
+ ];
+}
